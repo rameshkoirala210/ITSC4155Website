b62fc6be382fa9ab09cd713b81e504c614d0e174
Ramesh Koirala <rameshkoirala210@gmail.com>
2022-05-03 21:44:02 -0400

    Website Compare added

--- script.js
+++ script.js
@@ -1,23 +1,53 @@
 
-    let selection = document.querySelector('select');
-    let result = document.querySelector('h2');
+    var dict = {
+        Date2: "http://public.tableau.com/views/RegionalSampleWorkbook/Storms",
+        Date1: "https://public.tableau.com/views/Testing1_16492670078510/Sheet1?:language=en-US&:display_count=n&:origin=viz_share_link"
+    }
 
-    selection.addEventListener('change', () => {
-        result.innerText = selection.options[selection.selectedIndex].text;
-        console.log(selection.selectedIndex);
-        console.log(result.innerText);
-    });
-            
-    function initViz() {
-        var containerDiv = document.getElementById("vizContainer")
-        url = "http://public.tableau.com/views/RegionalSampleWorkbook/Storms";
-        //url = "https://public.tableau.com/views/Testing1_16492670078510/Sheet1?:language=en-US&:display_count=n&:origin=viz_share_link";
+    var viz;
 
-        options = {
+    function dateOnChange() {
+        //create stuff here
+        console.log("ON CHANGE");
+        var containerDiv = document.getElementById("vizContainer");
+        var selectedValue = document.getElementById("DateList").value;
+        console.log(dict[selectedValue]);
+
+        let options = {
             hideTabs: true,
             onFirstInteractive: function () {
-            console.log("finished loading.");
+                console.log("finished loading.");
+            }
+            //make it bigger
+        };
+
+        if (viz != null) {
+            viz.dispose();
         }
-    };
-    var viz = new tableau.Viz(containerDiv, url, options);
+        viz = new tableau.Viz(containerDiv, dict[selectedValue], options);
     }
+
+    var viz2;
+
+    function compareOnChange() {
+        //create stuff here
+        console.log("ON CHANGE");
+        var containerDiv = document.getElementById("vizCompareContainer");
+        var selectedValue = document.getElementById("CompareDateList").value;
+        console.log(dict[selectedValue]);
+
+        let options = {
+            hideTabs: true,
+            onFirstInteractive: function () {
+                console.log("finished loading.");
+            }
+            //make it bigger
+        };
+
+        if (viz2 != null) {
+            viz2.dispose();
+        }
+        viz2 = new tableau.Viz(containerDiv, dict[selectedValue], options);
+    }
+
+    //https://community.tableau.com/s/question/0D54T00000C5ja9SAB/another-viz-is-already-present-in-element-divtableauviz-error
