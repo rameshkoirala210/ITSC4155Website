0c7dc0a52fefed5e71c3dd4c2c17f69bca4fe607
Ramesh Koirala <rameshkoirala210@gmail.com>
2022-05-11 14:25:09 -0400

    ErrorLocationChartAdded

---
+++ errorLocation.js
@@ -0,0 +1,88 @@
+var dict = {
+    August2: "https://public.tableau.com/views/Aug02EbB/Sheet1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    August3: "https://public.tableau.com/views/Aug03EBB/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    August9: "https://public.tableau.com/views/Aug09EBB/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    August10: "https://public.tableau.com/views/Aug10EBB/Sheet1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    August16: "https://public.tableau.com/views/Aug16EBB/Sheet1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    August17: "https://public.tableau.com/views/Aug17EBB/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    August30: "https://public.tableau.com/views/Aug30EBB/Sheet1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    August31: "https://public.tableau.com/views/Aug31EBB/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    September6: "https://public.tableau.com/views/Sep06EBB/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    September7: "https://public.tableau.com/views/Sep07EBB/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    September13: "https://public.tableau.com/views/Sep13EBB/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    September14: "https://public.tableau.com/views/Sep14EBB/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    September20: "https://public.tableau.com/views/Sep20EBB/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    September21: "https://public.tableau.com/views/Sep21EBB/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    September27: "https://public.tableau.com/views/Sep27EBB/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    September28: "https://public.tableau.com/views/Sep28EBB/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    October4: "https://public.tableau.com/views/Oct04EBB/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    October5: "https://public.tableau.com/views/Oct05EBB/Sheet1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    October11: "https://public.tableau.com/views/Oct11EBB/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    October12: "https://public.tableau.com/views/Oct12EBB/Sheet1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    October18: "https://public.tableau.com/views/Oct18EBB/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    October19: "https://public.tableau.com/views/Oct19EBB/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    October25: "https://public.tableau.com/views/Oct25EBB/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    October26: "https://public.tableau.com/views/Oct26EBB/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    November1: "https://public.tableau.com/views/Nov1EBB/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    November2: "https://public.tableau.com/views/Nov02EBB/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    November8: "https://public.tableau.com/views/Nov08EBB/Sheet1?:language=en-US&:display_count=n&:origin=viz_share_link",
+    November9: "https://public.tableau.com/views/Nov09EBB/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    November15: "https://public.tableau.com/views/Nov15EBB/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    November16: "https://public.tableau.com/views/Nov16EBB/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    November22: "https://public.tableau.com/views/Nov22EBB/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    November23: "https://public.tableau.com/views/Nov23EBB/Sheet1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    November29: "https://public.tableau.com/views/Nov29EBB/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    November30: "https://public.tableau.com/views/Nov30EBB/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    December6: "https://public.tableau.com/views/Dec06EBB/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    December7: "https://public.tableau.com/views/Dec07EBB/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    December13: "https://public.tableau.com/views/Dec13EBB/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    December14: "https://public.tableau.com/views/Dec14EBB/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    December20: "https://public.tableau.com/views/Dec20EBB/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    December21: "https://public.tableau.com/views/Dec21EBB/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    December27: "https://public.tableau.com/views/Dec27EBB/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    December28: "https://public.tableau.com/views/Dec28EBB/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+}
+
+var viz;
+
+function dateOnChange() {
+    
+    console.log("ON CHANGE");
+    var containerDiv = document.getElementById("vizContainer");
+    var selectedValue = document.getElementById("DateList").value;
+    console.log(dict[selectedValue]);
+
+    let options = {
+        hideTabs: true,
+        onFirstInteractive: function () {
+            console.log("finished loading.");
+        }
+
+    };
+
+    if (viz != null) {
+        viz.dispose();
+    }
+    viz = new tableau.Viz(containerDiv, dict[selectedValue], options);
+}
+
+var viz2;
+
+function compareOnChange() {
+    console.log("ON CHANGE");
+    var containerDiv = document.getElementById("vizCompareContainer");
+    var selectedValue = document.getElementById("CompareDateList").value;
+    console.log(dict[selectedValue]);
+
+    let options = {
+        hideTabs: true,
+        onFirstInteractive: function () {
+            console.log("finished loading.");
+        }
+    };
+
+    if (viz2 != null) {
+        viz2.dispose();
+    }
+    viz2 = new tableau.Viz(containerDiv, dict[selectedValue], options);
+}
